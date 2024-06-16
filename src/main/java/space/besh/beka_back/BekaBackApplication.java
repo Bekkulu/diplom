dc9ac0138fe6eb562c70b8b7e55c501de1d1a65b
muh <muhtarkasimov@gmail.com>
2024-06-17 04:18:10 +0600

    main/ добавил аннотацию EnableJpaAudition для автоматической подставки даты присоздании\изменении entity

--- src/main/java/space/besh/beka_back/BekaBackApplication.java
+++ src/main/java/space/besh/beka_back/BekaBackApplication.java
@@ -2,8 +2,10 @@ package space.besh.beka_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BekaBackApplication {
 
     public static void main(String[] args) {
