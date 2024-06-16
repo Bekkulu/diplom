cb7d6bef79b84168b29cc5ffc54b3ee76a2bce32
muh <muhtarkasimov@gmail.com>
2024-06-17 04:19:22 +0600

    main/ исправили логику и модель создания goods закомментили пока изменение goods

--- src/main/java/space/besh/beka_back/repos/ProductTypeRepository.java
+++ src/main/java/space/besh/beka_back/repos/ProductTypeRepository.java
@@ -6,8 +6,10 @@ import space.besh.beka_back.entity.ProductType;
 
 import java.util.List;
 
-public interface ProductTypeRepository extends JpaRepository<ProductType,Long> {
+public interface ProductTypeRepository extends JpaRepository<ProductType, Long> {
 
     @Query("select pt from ProductType pt where (:#{#name} is null or pt.name ilike (concat('%', ?1,'%')))")
     List<ProductType> findAll(String name);
+
+    ProductType findById(int id);
 }
